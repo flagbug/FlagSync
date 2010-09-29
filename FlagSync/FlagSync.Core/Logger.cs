4fc7ee76888e391c40d873aa8f24d225696ee3e0
Dennis Daume <daume.dennis@gmail.com>
2010-09-29 12:56:24 +0000

    [ENH] Enhanced logger

--- FlagSync/FlagSync.Core/Logger.cs
+++ FlagSync/FlagSync.Core/Logger.cs
@@ -5,7 +5,12 @@ namespace FlagSync.Core
 {
     public class Logger
     {
-        private static Logger instance;
+        private string path = String.Empty;
+        private static Logger instance = null;
+
+        /// <summary>
+        /// Singleton instance of the logger (must be manually initialized)
+        /// </summary>
         public static Logger Instance
         {
             get
@@ -19,9 +24,9 @@ namespace FlagSync.Core
             }
         }
 
-        private string path = String.Empty;
-        private string fileName = String.Empty;
-
+        /// <summary>
+        /// Path of the log file
+        /// </summary>
         public string Path
         {
             get
@@ -30,38 +35,74 @@ namespace FlagSync.Core
             }
         }
 
-        public string FileName
-        {
-            get
-            {
-                return this.fileName;
-            }
-        }
-
+        /// <summary>
+        /// Creates a new instance of the logger, with the given filepath
+        /// </summary>
+        /// <param name="path">The path of the log file</param>
         public Logger(string path)
         {
             this.path = path;
-            File.Create(path);
+
+            try
+            {
+                File.Create(path);
+            }
+
+            catch(DirectoryNotFoundException e)
+            {
+                System.Diagnostics.Debug.WriteLine(e.Message);
+                throw;
+            }
         }
 
-        public void LogError(string log)
+        /// <summary>
+        /// Logs an error
+        /// </summary>
+        /// <param name="message">The error message</param>
+        public void LogError(string message)
         {
-            DateTime time = DateTime.Now;
+            this.Log(message, "ERROR");
+        }
 
+        /// <summary>
+        /// Logs a succeed message
+        /// </summary>
+        /// <param name="message">The succeed message</param>
+        public void LogSucceed(string message)
+        {
+            this.Log(message, "SUCCEED");
+        }
+
+        /// <summary>
+        /// Logs a status message
+        /// </summary>
+        /// <param name="message">The status message</param>
+        public void LogStatusMessage(string message)
+        {
+            this.Log(message, "STATUS");
+        }
+
+        /// <summary>
+        /// Adds a log
+        /// </summary>
+        /// <param name="log">The log message</param>
+        /// <param name="type">The message type, e.g "ERROR" or "SUCCEED"</param>
+        private void Log(string log, string type)
+        {
             StreamWriter writer = null;
 
             try
             {
-                writer = new StreamWriter(path, true);            
-                
-                writer.WriteLine(time.ToString() + " ERROR: " + log);
+                writer = new StreamWriter(path, true);
+
+                writer.WriteLine(DateTime.Now.ToString() + " " + type + ": " + log);
 
                 writer.Close();
             }
 
-            catch
+            catch(IOException e)
             {
-                throw;
+                System.Diagnostics.Debug.WriteLine(e.Message);
             }
 
             finally
