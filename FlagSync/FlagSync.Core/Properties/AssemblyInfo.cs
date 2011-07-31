e445ed82ae0cab07620c4976d0b0ff60521731a2
Dennis Daume <daume.dennis@gmail.com>
2011-07-31 17:24:52 +0000

    [RELEASE] Release of version 2.0.0

--- FlagSync/FlagSync.Core/Properties/AssemblyInfo.cs
+++ FlagSync/FlagSync.Core/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.3.3")]
+[assembly: AssemblyVersion("2.0.0")]
+[assembly: AssemblyFileVersion("2.0.0")]
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("FlagSync.Core.Test")]
