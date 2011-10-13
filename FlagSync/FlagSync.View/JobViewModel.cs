5e943338259ca7dce4444751d22d753e9c9fc27d
Dennis Daume <daume.dennis@gmail.com>
2011-10-13 14:49:38 +0200

    Added documentation for the "JobViewModel" class

--- FlagSync/FlagSync.View/JobViewModel.cs
+++ FlagSync/FlagSync.View/JobViewModel.cs
@@ -6,21 +6,34 @@ namespace FlagSync.View
     {
         private Job model;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JobViewModel"/> class.
+        /// </summary>
+        /// <param name="model">The model.</param>
         public JobViewModel(Job model)
         {
             this.model = model;
         }
 
+        /// <summary>
+        /// Gets the directory A.
+        /// </summary>
         public string DirectoryA
         {
             get { return this.model.DirectoryA.FullName; }
         }
 
+        /// <summary>
+        /// Gets the directory B.
+        /// </summary>
         public string DirectoryB
         {
             get { return this.model.DirectoryB.FullName; }
         }
 
+        /// <summary>
+        /// Gets the name.
+        /// </summary>
         public string Name
         {
             get { return this.model.Name; }
