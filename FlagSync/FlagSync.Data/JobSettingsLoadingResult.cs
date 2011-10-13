b1dcdb2ef5aa302c5d66cc1cc524f67c00e35cba
Dennis Daume <daume.dennis@gmail.com>
2011-10-13 15:08:42 +0200

    Code cleanup

--- FlagSync/FlagSync.Data/JobSettingsLoadingResult.cs
+++ FlagSync/FlagSync.Data/JobSettingsLoadingResult.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace FlagSync.Data
+﻿namespace FlagSync.Data
 {
     public enum JobSettingsLoadingResult
     {
--- FlagSync/FlagSync.View/MainViewModel.cs
+++ FlagSync/FlagSync.View/MainViewModel.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Net;
 using System.Reflection;
 using System.Windows;
 using System.Windows.Input;
