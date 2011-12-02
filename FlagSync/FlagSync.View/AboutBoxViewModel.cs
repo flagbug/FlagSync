0c4f9533e794bba2fb8b3b85b093f3b9ec5b39a0
Dennis Daume <daume.dennis@gmail.com>
2011-12-02 13:41:57 +0100

    Made the "Version", "Author" and "Title" property static. This fixes FxCop rule CA1822.

--- FlagSync/FlagSync.View/AboutBoxViewModel.cs
+++ FlagSync/FlagSync.View/AboutBoxViewModel.cs
@@ -8,7 +8,7 @@ namespace FlagSync.View
         /// Gets the application version.
         /// </summary>
         /// <value>The application version.</value>
-        public string Version
+        public static string Version
         {
             get { return Assembly.GetExecutingAssembly().GetName().Version.ToString(3); }
         }
@@ -17,7 +17,7 @@ namespace FlagSync.View
         /// Gets the author of the application
         /// </summary>
         /// <value>The author of the application.</value>
-        public string Author
+        public static string Author
         {
             get { return "Dennis Daume"; }
         }
@@ -26,7 +26,7 @@ namespace FlagSync.View
         /// Gets the title of the application.
         /// </summary>
         /// <value>The title of the application.</value>
-        public string Title
+        public static string Title
         {
             get { return "FlagSync"; }
         }
