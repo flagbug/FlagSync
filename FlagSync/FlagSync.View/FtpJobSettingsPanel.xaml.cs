5853bf22dbbdab313ecc79caf6ba77549e6b1ca1
Dennis Daume <daume.dennis@gmail.com>
2012-02-14 23:13:58 +0100

    Fixed the build

--- FlagSync/FlagSync.View/FtpJobSettingsPanel.xaml.cs
+++ FlagSync/FlagSync.View/FtpJobSettingsPanel.xaml.cs
@@ -1,50 +1,18 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
-using FlagSync.Data;
 
 namespace FlagSync.View
 {
     /// <summary>
     /// Interaction logic for FtpJobSettingsPanel.xaml
     /// </summary>
-    public sealed partial class FtpJobSettingsPanel : UserControl
+    public sealed partial class FtpJobSettingsPanel
     {
         public FtpJobSettingsPanel(JobSettingViewModel viewModel)
         {
             InitializeComponent();
 
             this.viewModel.JobSetting = viewModel;
-
-            switch (viewModel.SyncMode)
-            {
-                case SyncMode.FtpBackup:
-                    this.jobSettingsBackupModeRadioButton.IsChecked = true;
-                    break;
-
-                case SyncMode.FtpSynchronization:
-                    this.jobSettingsSyncModeRadioButton.IsChecked = true;
-                    break;
-            }
-        }
-
-        /// <summary>
-        /// Handles the Checked event of the jobSettingsBackupModeRadioButton control.
-        /// </summary>
-        /// <param name="sender">The source of the event.</param>
-        /// <param name="e">The <see cref="System.Windows.RoutedEventArgs"/> instance containing the event data.</param>
-        private void jobSettingsBackupModeRadioButton_Checked(object sender, RoutedEventArgs e)
-        {
-            this.viewModel.JobSetting.SyncMode = SyncMode.FtpBackup;
-        }
-
-        /// <summary>
-        /// Handles the Checked event of the jobSettingsSyncModeRadioButton control.
-        /// </summary>
-        /// <param name="sender">The source of the event.</param>
-        /// <param name="e">The <see cref="System.Windows.RoutedEventArgs"/> instance containing the event data.</param>
-        private void jobSettingsSyncModeRadioButton_Checked(object sender, RoutedEventArgs e)
-        {
-            this.viewModel.JobSetting.SyncMode = SyncMode.FtpSynchronization;
         }
 
         /// <summary>
@@ -55,42 +23,6 @@ namespace FlagSync.View
         private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
         {
             this.viewModel.JobSetting.FtpPassword = ((PasswordBox)sender).Password;
-        }
-
-        /// <summary>
-        /// Handles the Click event of the directoryButton control.
-        /// </summary>
-        /// <param name="sender">The source of the event.</param>
-        /// <param name="e">The <see cref="System.Windows.RoutedEventArgs"/> instance containing the event data.</param>
-        private void directoryButton_Click(object sender, RoutedEventArgs e)
-        {
-            string result = FtpJobSettingsPanel.ShowFolderDialog();
-
-            if (result != null)
-            {
-                this.viewModel.JobSetting.DirectoryB = result;
-            }
-        }
-
-        /// <summary>
-        /// Shows a folder dialog.
-        /// </summary>
-        /// <returns>The selected folder</returns>
-        private static string ShowFolderDialog()
-        {
-            string selectedFolder = null;
-
-            using (var dialog = new System.Windows.Forms.FolderBrowserDialog())
-            {
-                var result = dialog.ShowDialog();
-
-                if (result == System.Windows.Forms.DialogResult.OK)
-                {
-                    selectedFolder = dialog.SelectedPath;
-                }
-            }
-
-            return selectedFolder;
         }
     }
 }
--- FlagSync/FlagSync.View/ITunesJobSettingsPanel.xaml.cs
+++ FlagSync/FlagSync.View/ITunesJobSettingsPanel.xaml.cs
@@ -1,12 +1,9 @@
-﻿using System.Windows;
-using System.Windows.Controls;
-
-namespace FlagSync.View
+﻿namespace FlagSync.View
 {
     /// <summary>
     /// Interaction logic for ITunesJobSettingsPanel.xaml
     /// </summary>
-    public sealed partial class ITunesJobSettingsPanel : UserControl
+    public sealed partial class ITunesJobSettingsPanel
     {
         private readonly ITunesJobSettingsViewModel viewModel;
 
@@ -17,42 +14,6 @@ namespace FlagSync.View
             this.viewModel = new ITunesJobSettingsViewModel(viewModel);
 
             this.DataContext = this.viewModel;
-        }
-
-        /// <summary>
-        /// Handles the Click event of the directoryBButton control.
-        /// </summary>
-        /// <param name="sender">The source of the event.</param>
-        /// <param name="e">The <see cref="System.Windows.RoutedEventArgs"/> instance containing the event data.</param>
-        private void directoryButton_Click(object sender, RoutedEventArgs e)
-        {
-            string result = this.ShowFolderDialog();
-
-            if (result != null)
-            {
-                this.viewModel.JobSetting.DirectoryB = result;
-            }
-        }
-
-        /// <summary>
-        /// Shows a folder dialog.
-        /// </summary>
-        /// <returns>The selected folder</returns>
-        private string ShowFolderDialog()
-        {
-            string selectedFolder = null;
-
-            using (var dialog = new System.Windows.Forms.FolderBrowserDialog())
-            {
-                var result = dialog.ShowDialog();
-
-                if (result == System.Windows.Forms.DialogResult.OK)
-                {
-                    selectedFolder = dialog.SelectedPath;
-                }
-            }
-
-            return selectedFolder;
         }
     }
 }
