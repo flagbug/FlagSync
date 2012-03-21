14d9d9648b951687e9f29175ebe64fc42eec169c
Dennis Daume <daume.dennis@gmail.com>
2012-03-21 16:03:51 +0100

    Playing with the new job settings

---
+++ FlagSync/FlagSync.Data/LocalFileSystemSetting.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using FlagSync.Core.FileSystem.Base;
+using FlagSync.Core.FileSystem.Local;
+
+namespace FlagSync.Data
+{
+    public class LocalFileSystemSetting : FileSystemSetting
+    {
+        public override IFileSystem GetFileSystem()
+        {
+            return new LocalFileSystem();
+        }
+
+        public override IDirectoryInfo GetRootDirectory()
+        {
+            return new LocalDirectoryInfo(new DirectoryInfo(this.Source));
+        }
+    }
+}
--- FlagSync/FlagSync.Data/SyncMode.cs
+++ FlagSync/FlagSync.Data/SyncMode.cs
@@ -5,29 +5,7 @@
     /// </summary>
     public enum SyncMode
     {
-        /// <summary>
-        /// Backup on the local file system
-        /// </summary>
-        LocalBackup,
-
-        /// <summary>
-        /// Synchronization on the local file System
-        /// </summary>
-        LocalSynchronization,
-
-        /// <summary>
-        /// Backup on a FTP server
-        /// </summary>
-        FtpBackup,
-
-        /// <summary>
-        /// Synchronization on a FTP server
-        /// </summary>
-        FtpSynchronization,
-
-        /// <summary>
-        /// Synchronization of an extern media device with iTunes
-        /// </summary>
-        ITunes
+        Backup,
+        Synchronization
     }
 }
